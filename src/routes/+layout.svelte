32c021331035cf6840683f93ebe017db479308f6
“Tibebe <tibebe.demissie@matillion.com>
2024-10-03 22:49:16 +0100

    feat: create action to deploy to pages

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<main>
+    <h1>Welcome to GrimRepoer</h1>
+</main>
+    <slot></slot>
+
+<style>
+main{ 
+    width: 100vw;
+    height: 100vh;
+    background-color: black;
+    color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+}
+</style>
