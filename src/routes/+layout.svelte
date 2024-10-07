455ea91d6024926a5e683f50d9ea90ba30e84b40
“Tibebe <tibebe.demissie@matillion.com>
2024-10-07 01:25:37 +0100

    fix: attempt to fix url issure

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,21 @@
-
+<script>
+	export const trailingSlash = 'always';
+</script>
 
 <main>
-    <h1>Welcome to GrimRepoer</h1>
-    <slot></slot>
-
+	<h1>Welcome to GrimRepoer</h1>
+	<slot></slot>
 </main>
 
 <style>
-
-main{ 
-    width: 100vw;
-    height: 100vh;
-    background-color:#36373a;
-    color: white;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-}
+	main {
+		width: 100vw;
+		height: 100vh;
+		background-color: #36373a;
+		color: white;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;
+	}
 </style>
--- svelte.config.js
+++ svelte.config.js
@@ -14,8 +14,14 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			fallback: '404.html'
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false
 		}),
+		prerender: {
+			default: true,
+		},
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
